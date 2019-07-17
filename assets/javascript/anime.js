a1c5acdc9138bfd9a5570b705d0a56a97d12d49c
Ubernator <43094772+izzydavid@users.noreply.github.com>
2019-07-16 21:31:43 -0400

    Added Two Buttons after Play Button for Movies or Shows Options and Game begins

--- assets/javascript/anime.js
+++ assets/javascript/anime.js
@@ -1,13 +1,37 @@
 $('#playButton').hide(); 
-$('strong').hide()
-$('#blanks').hide()
-$('#animeTitle').hide()
-$('#playButton').delay(26000).fadeIn('slow');
+$('strong').hide();
+$('#blanks').hide();
+$('#animeTitle').hide();
+$('#myBtn').hide(); 
+$('#myBtn2').hide(); 
+$('#animePick').hide(); 
+$('#playButton').delay(200).fadeIn('slow'); 
 
 
 $("#playButton").on("click", function () {
+	$('#myBtn').delay(200).fadeIn('slow');
+	$('#myBtn2').delay(200).fadeIn('slow'); 
+	$('#animePick').delay(200).fadeIn('slow'); 
+	$('#playButton').remove(); 
+  });
+
+  $("#myBtn").on("click", function () {
 	$('strong').delay(200).fadeIn('slow');
 	$('#blanks').delay(200).fadeIn('slow'); 
-	$("#animeTitle").delay(200).fadeIn('slow'); 
-	$('#playButton').remove(); 
+	$('#animeTitle').delay(200).fadeIn('slow'); 
+	$('#myBtn').remove(); 
+	$('#myBtn2').remove(); 
+	$('#animePick').remove(); 
+
+  });
+
+  $("#myBtn2").on("click", function () {
+	$('strong').delay(200).fadeIn('slow');
+	$('#blanks').delay(200).fadeIn('slow'); 
+	$('#animeTitle').delay(200).fadeIn('slow'); 
+	$('#myBtn2').remove(); 
+	$('#myBtn').remove(); 
+	$('#animePick').remove(); 
+
+
   });
