6907ccb55c3b3b9acdbaeabdc10effed246fe4ca
IzzyD <davidirizarry@Davids-MacBook-Pro.local>
2019-08-13 01:15:52 -0400

    Fixing that it works with url for homework

--- assets/javascript/anime.js
+++ assets/javascript/anime.js
@@ -135,69 +135,69 @@ $("#myBtn3, #myBtn4, #myBtn5, #myBtn6").on("click", function intiialButtons() {
 
 
 
-var animate = function () {
-    var drawMe = lives ;
-    drawArray[drawMe]();
-  }
+// var animate = function () {
+//     var drawMe = lives ;
+//     drawArray[drawMe]();
+//   }
 
-canvas =  function(){
+// canvas =  function(){
 
-    myStickman = document.getElementById("stickman");
-    context = myStickman.getContext('2d');
-    context.beginPath();
-    context.strokeStyle = "#fff";
-    context.lineWidth = 2;
-  };
+//     myStickman = document.getElementById("stickman");
+//     context = myStickman.getContext('2d');
+//     context.beginPath();
+//     context.strokeStyle = "#fff";
+//     context.lineWidth = 2;
+//   };
   
-    head = function(){
-      myStickman = document.getElementById("stickman");
-      context = myStickman.getContext('2d');
-      context.beginPath();
-      context.arc(60, 25, 10, 0, Math.PI*2, true);
-      context.stroke();
-    }
+//     head = function(){
+//       myStickman = document.getElementById("stickman");
+//       context = myStickman.getContext('2d');
+//       context.beginPath();
+//       context.arc(60, 25, 10, 0, Math.PI*2, true);
+//       context.stroke();
+//     }
     
-  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+//   draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
     
-    context.moveTo($pathFromx, $pathFromy);
-    context.lineTo($pathTox, $pathToy);
-    context.stroke(); 
-}
+//     context.moveTo($pathFromx, $pathFromy);
+//     context.lineTo($pathTox, $pathToy);
+//     context.stroke(); 
+// }
 
-   frame1 = function() {
-     draw (0, 150, 150, 150);
-   };
+//    frame1 = function() {
+//      draw (0, 150, 150, 150);
+//    };
    
-   frame2 = function() {
-     draw (10, 0, 10, 600);
-   };
+//    frame2 = function() {
+//      draw (10, 0, 10, 600);
+//    };
   
-   frame3 = function() {
-     draw (0, 5, 70, 5);
-   };
+//    frame3 = function() {
+//      draw (0, 5, 70, 5);
+//    };
   
-   frame4 = function() {
-     draw (60, 5, 60, 15);
-   };
+//    frame4 = function() {
+//      draw (60, 5, 60, 15);
+//    };
   
-   torso = function() {
-     draw (60, 36, 60, 70);
-   };
+//    torso = function() {
+//      draw (60, 36, 60, 70);
+//    };
   
-   rightArm = function() {
-     draw (60, 46, 100, 50);
-   };
+//    rightArm = function() {
+//      draw (60, 46, 100, 50);
+//    };
   
-   leftArm = function() {
-     draw (60, 46, 20, 50);
-   };
+//    leftArm = function() {
+//      draw (60, 46, 20, 50);
+//    };
   
-   rightLeg = function() {
-     draw (60, 70, 100, 100);
-   };
+//    rightLeg = function() {
+//      draw (60, 70, 100, 100);
+//    };
   
-   leftLeg = function() {
-     draw (60, 70, 20, 100);
-   };
+//    leftLeg = function() {
+//      draw (60, 70, 20, 100);
+//    };
   
-  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+//   drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
