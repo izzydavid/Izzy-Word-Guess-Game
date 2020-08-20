984c1f4b21103360587e81714d84de8753f26b9e
IzzyDavid <david.irizarry86@yahoo.com>
2020-08-20 11:16:31 -0400

    Fixed Carousel

--- assets/javascript/intro.js
+++ assets/javascript/intro.js
@@ -110,7 +110,7 @@ fetch('https://api.giphy.com/v1/gifs/search?q=')
 		console.log(response.data[i].images.original.url);	
 		$(".carousel").prepend(`
      	<div class="carousel-item"> 
-		<img class="card-image hoverable" src='${response.data[i].images.fixed_height_still.url}', 
+		<img class="card card-image hoverable" src='${response.data[i].images.fixed_height_still.url}', 
 		data-still='${response.data[i].images.fixed_height_still.url}'
 		data-animate='${response.data[i].images.fixed_height.url}'
 		data-state='still'>
@@ -118,7 +118,7 @@ fetch('https://api.giphy.com/v1/gifs/search?q=')
         </div>`);
 		M.AutoInit();
 	}
-		$(".card-image").hover(function () {
+		$("img").hover(function hoverImage () {
 		//The attr jQuery method allows us to get or set the value of any attribute on our HTML element
 		var state = $(this).attr("data-state");
 		//If the clicked image's state is still, update its src attribute to what its data-animate value is. Then, set the image's data-state to animate. Else set src to the data-still value
@@ -136,3 +136,5 @@ fetch('https://api.giphy.com/v1/gifs/search?q=')
 }
 addGifyButtons(); 
 winGame();
+showButtons();
+hoverImage();
